02034b08d6af758e6ee2f57f03534b21b3303f4d
evertlammerts <evertlammerts@users.noreply.github.com>
2025-02-07 08:53:01 +0000

    Deploying to gh-pages from @ evertlammerts/evertlammerts.github.io@0908d77776c1d03cd52a38d30a7c9f3d5aa99995 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -111,17 +111,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/vega-lite/";
         
       },
-    },{id: "post-a-post-with-geojson",
-      
-        title: "a post with geojson",
-      
-      description: "this is what included geojson code could look like",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2024/geojson-map/";
-        
-      },
     },{id: "post-a-post-with-echarts",
       
         title: "a post with echarts",
@@ -153,17 +142,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2023/tikzjax/";
-        
-      },
-    },{id: "post-a-post-with-bibliography",
-      
-        title: "a post with bibliography",
-      
-      description: "an example of a blog post with bibliography",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2023/post-bibliography/";
         
       },
     },{id: "post-a-post-with-jupyter-notebook",
@@ -274,17 +252,6 @@ ninja.data = [{
       handler: () => {
         
           window.location.href = "/blog/2021/diagrams/";
-        
-      },
-    },{id: "post-a-distill-style-blog-post",
-      
-        title: "a distill-style blog post",
-      
-      description: "an example of a distill-style blog post and main elements",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2021/distill/";
         
       },
     },{id: "post-a-post-with-twitter",
